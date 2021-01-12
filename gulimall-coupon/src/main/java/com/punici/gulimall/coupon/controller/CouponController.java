fdf81264a7e6b1c5bea21aea826ca8c08c789dc6
murloc <punici@163.com>
2021-01-12 19:39:35 +0800

    基础架构2

--- gulimall-coupon/src/main/java/com/punici/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/punici/gulimall/coupon/controller/CouponController.java
@@ -1,21 +1,17 @@
 package com.punici.gulimall.coupon.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.punici.gulimall.coupon.entity.CouponEntity;
-import com.punici.gulimall.coupon.service.CouponService;
 import com.punici.gulimall.common.utils.PageUtils;
 import com.punici.gulimall.common.utils.R;
+import com.punici.gulimall.coupon.entity.CouponEntity;
+import com.punici.gulimall.coupon.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
 
-
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * 优惠券信息
@@ -25,65 +21,95 @@ import com.punici.gulimall.common.utils.R;
  * @date 2021-01-10 21:06:20
  */
 @RestController
+@RefreshScope
 @RequestMapping("coupon/coupon")
-public class CouponController {
+public class CouponController
+{
     @Autowired
     private CouponService couponService;
-
+    
+    @Value("${gulimall.user}")
+    String user;
+    
+    @Value("${gulimall.age}")
+    int age;
+    
+    @GetMapping("test")
+    public R test()
+    {
+        return Objects.requireNonNull(R.ok().put("name", user)).put("age", age);
+    }
+    
+    @GetMapping("/member/coupons")
+    public R memberCoupons()
+    {
+        CouponEntity entity = new CouponEntity();
+        entity.setId(0L);
+        entity.setCouponType(0);
+        entity.setCouponImg("");
+        entity.setCouponName("10086");
+        return R.ok().put("coupons", Arrays.asList(entity));
+        
+    }
+    
     /**
      * 列表
      */
     @RequestMapping("/list")
-    //@RequiresPermissions("coupon:coupon:list")
-    public R list(@RequestParam Map<String, Object> params){
+    // @RequiresPermissions("coupon:coupon:list")
+    public R list(@RequestParam Map<String, Object> params)
+    {
         PageUtils page = couponService.queryPage(params);
-
+        
         return R.ok().put("page", page);
     }
-
-
+    
     /**
      * 信息
      */
     @RequestMapping("/info/{id}")
-    //@RequiresPermissions("coupon:coupon:info")
-    public R info(@PathVariable("id") Long id){
-		CouponEntity coupon = couponService.getById(id);
-
+    // @RequiresPermissions("coupon:coupon:info")
+    public R info(@PathVariable("id") Long id)
+    {
+        CouponEntity coupon = couponService.getById(id);
+        
         return R.ok().put("coupon", coupon);
     }
-
+    
     /**
      * 保存
      */
     @RequestMapping("/save")
-    //@RequiresPermissions("coupon:coupon:save")
-    public R save(@RequestBody CouponEntity coupon){
-		couponService.save(coupon);
-
+    // @RequiresPermissions("coupon:coupon:save")
+    public R save(@RequestBody CouponEntity coupon)
+    {
+        couponService.save(coupon);
+        
         return R.ok();
     }
-
+    
     /**
      * 修改
      */
     @RequestMapping("/update")
-    //@RequiresPermissions("coupon:coupon:update")
-    public R update(@RequestBody CouponEntity coupon){
-		couponService.updateById(coupon);
-
+    // @RequiresPermissions("coupon:coupon:update")
+    public R update(@RequestBody CouponEntity coupon)
+    {
+        couponService.updateById(coupon);
+        
         return R.ok();
     }
-
+    
     /**
      * 删除
      */
     @RequestMapping("/delete")
-    //@RequiresPermissions("coupon:coupon:delete")
-    public R delete(@RequestBody Long[] ids){
-		couponService.removeByIds(Arrays.asList(ids));
-
+    // @RequiresPermissions("coupon:coupon:delete")
+    public R delete(@RequestBody Long[] ids)
+    {
+        couponService.removeByIds(Arrays.asList(ids));
+        
         return R.ok();
     }
-
+    
 }
--- gulimall-member/src/main/java/com/punici/gulimall/member/controller/MemberController.java
+++ gulimall-member/src/main/java/com/punici/gulimall/member/controller/MemberController.java
@@ -1,20 +1,16 @@
 package com.punici.gulimall.member.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.punici.gulimall.member.entity.MemberEntity;
-import com.punici.gulimall.member.service.MemberService;
 import com.punici.gulimall.common.utils.PageUtils;
 import com.punici.gulimall.common.utils.R;
+import com.punici.gulimall.member.entity.MemberEntity;
+import com.punici.gulimall.member.feign.CouponFeignService;
+import com.punici.gulimall.member.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
 
 
 /**
@@ -29,6 +25,17 @@ import com.punici.gulimall.common.utils.R;
 public class MemberController {
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private CouponFeignService couponFeignService;
+
+    @GetMapping("/test")
+    public R test(){
+        MemberEntity memberEntity=new MemberEntity();
+        memberEntity.setNickname("张三李四");
+        R r = couponFeignService.memberCoupons();
+        return Objects.requireNonNull(R.ok().put("member", memberEntity)).put("coupons",r.get("coupons"));
+    }
 
     /**
      * 列表
