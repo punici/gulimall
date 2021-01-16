07bee9a0d8d62209bfea14b4614a12b8ceed919e
murloc <punici@163.com>
2021-01-16 19:04:19 +0800

    gateway

--- gulimall-product/src/main/java/com/punici/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/punici/gulimall/product/controller/CategoryController.java
@@ -1,19 +1,14 @@
 package com.punici.gulimall.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.punici.gulimall.common.utils.R;
 import com.punici.gulimall.product.entity.CategoryEntity;
 import com.punici.gulimall.product.service.CategoryService;
-import com.punici.gulimall.common.utils.PageUtils;
-import com.punici.gulimall.common.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 
 
@@ -31,15 +26,14 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子分类列表，以树形结构组装起来
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/tree")
     //@RequiresPermissions("product:category:list")
     public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
-    }
+       List<CategoryEntity> entities=  categoryService.listWithTree();
+        return R.ok().put("data", entities);
+   }
 
 
     /**
--- gulimall-product/src/main/java/com/punici/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/punici/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,29 +1,50 @@
 package com.punici.gulimall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.punici.gulimall.common.utils.PageUtils;
 import com.punici.gulimall.common.utils.Query;
-
 import com.punici.gulimall.product.dao.CategoryDao;
 import com.punici.gulimall.product.entity.CategoryEntity;
 import com.punici.gulimall.product.service.CategoryService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service("categoryService")
-public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
-
+public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService
+{
+    
     @Override
-    public PageUtils queryPage(Map<String, Object> params) {
-        IPage<CategoryEntity> page = this.page(
-                new Query<CategoryEntity>().getPage(params),
-                new QueryWrapper<CategoryEntity>()
-        );
-
+    public PageUtils queryPage(Map<String, Object> params)
+    {
+        IPage<CategoryEntity> page = this.page(new Query<CategoryEntity>().getPage(params), new QueryWrapper<CategoryEntity>());
+        
         return new PageUtils(page);
     }
-
+    
+    @Override
+    public List<CategoryEntity> listWithTree()
+    {
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        if(CollectionUtils.isEmpty(entities))
+        {
+            return entities;
+        }
+        return entities.stream().filter(o -> o.getParentCid().equals(0L)).peek(c -> c.setChildren(getChildren(c, entities)))
+                .sorted(Comparator.comparingInt(c -> ((c.getSort() == null) ? 0 : c.getSort()))).collect(Collectors.toList());
+    }
+    
+    private List<CategoryEntity> getChildren(CategoryEntity entity, List<CategoryEntity> entities)
+    {
+        return entities.stream().filter(e -> e.getParentCid().equals(entity.getCatId()))
+                .peek(c -> c.setChildren(getChildren(c, entities)))
+                .sorted(Comparator.comparingInt(c -> ((c.getSort() == null) ? 0 : c.getSort()))).collect(Collectors.toList());
+    }
+    
 }
